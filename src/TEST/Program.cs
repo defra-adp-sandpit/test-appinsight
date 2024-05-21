e72eba0f17ace6d0b407ffec65b7fdbe8f66319d
rajeshkaremane <rajesh.venkatraman@capgemini.com>
2024-05-21 10:14:22 +0100

    test with resource id

--- src/TEST/Program.cs
+++ src/TEST/Program.cs
@@ -11,13 +11,16 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddOpenTelemetry().UseAzureMonitor(o =>
             {
-                string userAssignedClientId = "9e5badc8-bb65-4b34-835b-ab2f5117e88e";
+                //string userAssignedClientId = "9e5badc8-bb65-4b34-835b-ab2f5117e88e";
                 o.ConnectionString = builder.Configuration.GetValue<string>("AppInsights:ConnectionString");
-                o.Credential = new DefaultAzureCredential(
-                new DefaultAzureCredentialOptions
-                {
-                    ManagedIdentityClientId = userAssignedClientId
-                });
+                string resourid = "/subscriptions/7dc5bbdf-72d7-42ca-ac23-eb5eea3764b4/resourcegroups/SSVADPDEVRG3401/providers/Microsoft.ManagedIdentity/userAssignedIdentities/SSVADPDEVMI3401-adp-portal-web";
+                o.Credential = new ManagedIdentityCredential(resourid, null);
+                new ManagedIdentityCredential();
+                // o.Credential = new DefaultAzureCredential(
+                // new DefaultAzureCredentialOptions
+                // {
+                //     ManagedIdentityClientId = userAssignedClientId
+                // });
             });
 builder.Services.ConfigureOpenTelemetryTracerProvider((sp, b) =>
             {
